442c0f62f9f1cba7c6cd0201e22a540de82bd140
UltimatePea <superchenzb@gmail.com>
2017-12-30 17:48:36 -0800

    Add Missing files

---
+++ Assets/DataModel.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/* Roads are attached to the from intersection */
+public class Road
+{
+	public Intersection fromIntersection;
+	public Intersection toIntersection;
+
+	enum IntersectionClass
+	{
+FROM,
+		TO,
+		NONE
+/* Intersection ot part of this road */}
+
+	;
+
+	public IntersectionClass typeOfIntersection (Intersection intersection)
+	{
+		if (fromIntersection == intersection) {
+			return IntersectionClass.FROM;
+		} else if (toIntersection == intersection) {
+			return IntersectionClass.TO;
+		} else {
+			return IntersectionClass.NONE;
+		}
+	}
+}
+
+public class Intersection
+{
+	private ArrayList connectedRoads;
+	private Vector3 _position;
+
+	public Vector3 position {
+		get { return _position; }
+		set {
+			_position = value;
+			// TODO update all roads mesh and this's mesh;
+		}
+	}
+
+	private GameObject gameObject; // the game object we own
+
+	// reference to the global builder
+	private IntersectionBuilder builder;
+
+	public Intersection (Vector3 position, Road[] connectedRoads = {})
+	{
+		this.connectedRoads = new ArrayList (connectedRoads);
+		this.position = position;
+
+		this.builder = GameObject.FindWithTag ("Builder").GetComponent<IntersectionBuilder> ();
+		this.gameObject = this.builder.BuildIntersection(this);
+
+	}
+
+	public Road[] getConnectedRoads ()
+	{
+		return this.connectedRoads.ToArray ();
+	}
+}
+
+public class DataModel : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
---
+++ Assets/IntersectionBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class IntersectionBuilder : MonoBehaviour
+{
+
+	public GameObject fourWayIntersectionPrefab;
+	public GameObject oneWayIntersectionPrefab;
+
+
+	// build a single intersection, returns the created game object
+	public GameObject BuildIntersection (Intersection intersection)
+	{
+		Road[] roads = intersection.getConnectedRoads ();
+		// no intersection if no roads
+		if (roads.Length == 0) {
+			return Instantiate(oneWayIntersectionPrefab, )
+		}
+		
+	}
+
+}
