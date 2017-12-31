dd386293224b0b67bb90c9f6efae5ddad917e202
UltimatePea <superchenzb@gmail.com>
2017-12-31 15:11:15 -0800

    Add Multiway Intersection

--- Assets/Scripts/Builders/IntersectionBuilder.cs
+++ Assets/Scripts/Builders/IntersectionBuilder.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using System;
 
 
 
@@ -32,7 +34,7 @@ public class IntersectionBuilder : MonoBehaviour
 		} else 
 
 		// this is one way intersection
-		if (roads.Length == 1) {
+			if (roads.Length == 1 /* temp */ || roads.Length == 2 /* temp */) {
 			return buildOneWayIntersection (intersection, roads [0]);
 		} else {
 			return buildMultiwayIntersection (intersection, roads);
@@ -157,10 +159,117 @@ public class IntersectionBuilder : MonoBehaviour
 		return intersectionObj;
 	}
 
+
 	private GameObject buildMultiwayIntersection (Intersection intersection, Road[] connectedRoads)
 	{
-		// TODO
+		// get an angle of all roads
+		Road[] sorted = connectedRoads.OrderBy (rd => getAngle (intersection, rd).eulerAngles.y).ToArray ();
 
-		return null;
+		Mesh mesh = new Mesh ();
+
+		// get a list of intersection vertices
+		Vector3[] vertices = new Vector3[sorted.Length + 2];
+		float roadWidth = 1f;
+		for (int i = 0; i < sorted.Length; i++) {
+			Road rd1 = sorted [i];
+			Road rd2 = sorted [(i + 1) % sorted.Length];
+			// get intersection of rd1's right edge with rd2's left edge
+			Vector3 vec1 = getOutgoingVector (intersection, rd1).normalized;
+			Vector3 vec2 = getOutgoingVector (intersection, rd2).normalized;
+			Vector3 vec1Norm = Quaternion.Euler (new Vector3 (0, 90, 0)) * vec1 * roadWidth / 2;
+			Vector3 vec2Norm = Quaternion.Euler (new Vector3 (0, -90, 0)) * vec2 * roadWidth / 2;
+
+			bool res = Math3d.ClosestPointsOnTwoLines (out vertices [i], out vertices [i], vec1Norm, vec1, vec2Norm, vec2);
+			if (res == false) {
+				Debug.LogWarning ("Error when creating intersection: Line intersection does not exist");
+			}
+			Debug.LogFormat ("vec1 = {2}, vec2 = {3}, Vertex ({0}) : {1}", i, vertices [i], vec1, vec2);
+		}
+		vertices [sorted.Length] = vertices [0]; // the second last vertex is the first (for UV)
+		vertices [sorted.Length + 1] = Vector3.zero; // the last vertex is the intersection center
+		Debug.LogFormat ("Vertex ({0}) : {1}", sorted.Length, vertices [sorted.Length]);
+		mesh.vertices = vertices;
+
+		int[] triangles = new int[connectedRoads.Length * 3];
+		// TODO : Figure out how sortBy works, thus affecting the orientation of the plane
+		for (int i = 0; i < sorted.Length; i++) {
+			triangles [i * 3 + 2] = i;
+			triangles [i * 3 + 1] = sorted.Length + 1;
+			triangles [i * 3 + 0] = i + 1;
+		}
+		mesh.triangles = triangles;
+
+		Vector2[] uv = new Vector2[vertices.Length];
+		for (int i = 0; i < sorted.Length + 1; i++) {
+			switch (i % 4) {
+			case 0:
+				uv [i] = new Vector2 (0, 0);
+				break;
+			case 1:
+				uv [i] = new Vector2 (0, 1);
+				break;
+			case 2:
+				uv [i] = new Vector2 (1, 1);
+				break;
+			case 3:
+				uv [i] = new Vector2 (1, 0);
+				break;
+			}
+		}
+		uv [sorted.Length + 1] = new Vector2 (0.5f, 0.5f);
+		mesh.uv = uv;
+
+		Vector3[] normals = new Vector3[uv.Length];
+		for (int i = 0; i < normals.Length; i++) {
+			normals [i] = Vector3.up;
+		}
+		mesh.normals = normals;
+
+
+		// move up a little bit
+		// TODO Fix magic numbers
+		Vector3 position = intersection.position + new Vector3 (0, 0.02f, 0);
+
+		GameObject intersectionObj = Instantiate (fourWayIntersectionPrefab, position, Quaternion.identity);
+		MeshFilter objMeshFilter = intersectionObj.GetComponent<MeshFilter> ();
+		objMeshFilter.mesh = mesh;
+
+		MeshCollider objMeshCollider = intersectionObj.GetComponent<MeshCollider> ();
+		objMeshCollider.sharedMesh = mesh;
+
+		return intersectionObj;
+	}
+
+	// TODO Combine below two methods
+	Vector3 getOutgoingVector (Intersection intersection, Road road)
+	{
+		Vector3 vec = Vector3.zero;
+		switch (road.typeOfIntersection (intersection)) {
+		case Road.IntersectionClass.FROM:
+			vec = road.toIntersection.position - road.fromIntersection.position;
+			break;
+		case Road.IntersectionClass.TO:
+			vec = road.fromIntersection.position - road.toIntersection.position;
+			break;
+		}
+	
+		return vec;
+		
+	}
+
+	Quaternion getAngle (Intersection intersection, Road road)
+	{
+		Quaternion angle = Quaternion.identity;
+		switch (road.typeOfIntersection (intersection)) {
+		case Road.IntersectionClass.FROM:
+			angle = Quaternion.FromToRotation (Vector3.right, road.toIntersection.position - road.fromIntersection.position);
+			break;
+		case Road.IntersectionClass.TO:
+			angle = Quaternion.FromToRotation (Vector3.right, road.fromIntersection.position - road.toIntersection.position);
+			break;
+		}
+	
+		return angle;
+		
 	}
 }
