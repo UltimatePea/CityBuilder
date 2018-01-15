d14e78a20e1e12baf12c60f241cc2c796cb753c5
UltimatePea <superchenzb@gmail.com>
2018-01-14 20:14:54 -0800

    Add Doxygen docs and CI

---
+++ travis_scripts/install.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+
+#https://stablekernel.com/continuous-integration-for-unity-5-using-travisci/
+BASE_URL=https://download.unity3d.com/download_unity
+HASH=94bf3f9e6b5e
+VERSION=2017.2.1f1
+
+download() {
+  file=$1
+  url="$BASE_URL/$HASH/$package"
+
+  echo "Downloading from $url: "
+  curl -o `basename "$package"` "$url"
+}
+
+install() {
+  package=$1
+  download "$package"
+
+  echo "Installing "`basename "$package"`
+  sudo installer -dumplog -package `basename "$package"` -target /
+}
+
+# See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
+# of available packages, where PLATFORM is `osx` or `win`
+
+install "MacEditorInstaller/Unity-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-$VERSION.pkg"
